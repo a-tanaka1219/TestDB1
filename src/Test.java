b2721d3cb52efd3f5d6d2308068a7f8231852c1e
田中彩美紗 <amisasm@tanakaayamishanoMacBook-Air.local>
2024-05-03 11:11:40 +0900

    演習10

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,8 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 public class Test {
 	public static void main(String[] args){
@@ -7,5 +12,30 @@ public class Test {
 		dao.selectByName("taro");
 		dao.selectByPassword("123");
 	}
+
+
+
+public void selectByPassword(String password){
+	DBConnector db = new DBConnector();
+	Connection con = db.getConnection();
+	
+	String sql = "select * from test_table where password=?";
+	try{
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString((1, password);
+		ResultSet rs=ps.executeQuery();
+		while (rs.next()){
+			System.out.println(rs.getString("user_name"));
+			System.out.println(rs.getString("password"));
+		}
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+	try{
+		con.close();
+	}catch (SQLException e){
+		e.printStackTrace();
+	}
+}
 
 }
